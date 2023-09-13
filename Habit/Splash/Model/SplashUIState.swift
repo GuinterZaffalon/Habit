a3a2f14b88f4f8609adaffd2e2a577573eba02ff
Guinter Zaffalon <guinterzafa@gmail.com>
2023-09-13 19:25:41 -0300

    Criação da estrutura do projeto
    Criação da estrutura e organização dos estados iniciais classificados na sintaxe "enum"!

---
+++ Habit/Splash/Model/SplashUIState.swift
@@ -0,0 +1,15 @@
+//
+//  SplashUIState.swift
+//  Habit
+//
+//  Created by Guinter Zaffalon on 13/09/23.
+//
+
+import Foundation
+
+enum SplashUIState{
+    case loading
+    case goToSignInScreen
+    case goToHomeScreen
+    case error(String)
+}
---
+++ Habit/Splash/View/SplashView.swift
@@ -0,0 +1,24 @@
+//
+//  SplashView.swift
+//  Habit
+//
+//  Created by Guinter Zaffalon on 13/09/23.
+//
+
+import SwiftUI
+
+struct SplashView: View{
+    
+    @State
+    
+    var body: some View {
+        Text("Olá")
+    }
+}
+
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
