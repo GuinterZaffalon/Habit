31fde43c56bab25c5ad247918929ea8d92f64ded
Guinter Zaffalon <guinterzafa@gmail.com>
2023-09-13 15:17:02 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Guinter Zaffalon on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
